38e66b99daf7b3a1718c6962258e06d11e8050a9
Splinter0 <splint@protonmail.com>
2019-01-04 17:29:44 +0100

    New version

--- tonno.py
+++ tonno.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import time
+import argparse
 import threading
-import schedule
 import random
+from tqdm import tqdm
 from collections import OrderedDict
 from huepy import bold, green, orange
 sys.path.append(os.path.join(sys.path[0], 'instabot/'))
@@ -12,9 +14,9 @@ from instabot import Bot
 
 username = ""
 password = ""
-influencers = "influencers.txt"
+
+influencers = "influencers/targets.txt"
 toFollow = "toFollow.txt"
-followNum = 2000
 
 
 # FUNCS
@@ -46,17 +48,43 @@ class TonnoBot(object):
         else:
             return 0
 
-    def createFollowers(self, inf, save):
-        self.influencers = self.bot.read_list_from_file(inf)
+    def threadUsers(self, l):
+        d = len(l) / 4
+        self.threads = []
+        self.threads.append(UserLoader(l[:d], self.bot))
+        self.threads.append(UserLoader(l[d:(d*2)], self.bot))
+        self.threads.append(UserLoader(l[(d*2):(d*3)], self.bot))
+        self.threads.append(UserLoader(l[(d*3):-1], self.bot))
+
+        for t in self.threads:
+            t.start()
+
+        while self.threads:
+            for t in self.threads:
+                if t.stopped():
+                    self.threads.remove(t)
+                    self.targets += t.users
+
+            time.sleep(5)
+
+
+    def createFollowers(self, save):
         self.tmp = []
         for i in self.influencers:
-            self.tmp += self.bot.get_user_followers(i)
+            users = self.bot.get_user_followers(i)
+            if len(self.tmp) > 0:
+                ex = list(set(users) - set(self.tmp))
+                self.tmp += ex
+            else:
+                self.tmp += users
 
         self.targets = []
-        for t in self.tmp:
-            user_id = self.convert_to_user_id(t)
-            if self.bot.check_user(user_id):
-                self.targets.append(User(user_id, self.bot.get_user_info(user_id)))
+        self.bot.verbosity = False
+        self.threadUsers(self.tmp)
+        #self.bot.verbosity = True
+        for t in self.targets:
+            if t.id in self.bot.skipped_file or t.id in self.bot.unfollowed_file:
+                self.targets.remove(t)
 
         self.targets = sorted(self.targets, key= lambda t: self.scoreUser(t), reverse=True)
         if len(self.targets) > self.followNum:
@@ -67,16 +95,68 @@ class TonnoBot(object):
         print("Saving targets to "+save)
         self.targetsFile = File(save)
         self.targetsFile.save_list(self.targets)
+        done = File("influencers/done.txt")
+        done.save_list(self.influencers)
 
     def massFollow(self, load=None):
         if load != None:
             self.targets = self.bot.read_list_from_file(load)
         self.bot.follow_users(self.targets)
 
+    def massUnfollow(self, load=None):
+        if load != None:
+            self.targets = self.bot.read_list_from_file(load)
+        self.bot.unfollow_users(self.targets)
 
-    def run_threaded(self, job_fn):
-        job_thread = threading.Thread(target=job_fn)
-        job_thread.start()
+    def followPhase(self, mixed=False):
+        print("Follow phase")
+        self.influencers = self.bot.read_list_from_file("influencers/targets.txt")
+        done = self.bot.read_list_from_file("influencers/done.txt")
+        for i in self.influencers:
+            if i in done:
+                self.influencers.remove(i)
+
+        if len(self.influencers) > 0:
+            if not mixed:
+                self.influencers = [self.influencers[0]]
+            start = time.time()
+            self.createFollowers(toFollow)
+            print("Loading all "+str(len(self.targets))+" users took : " + str(time.time() - start))
+            start = time.time()
+            self.massFollow()
+            print("Following all "+str(len(self.targets))+" users took : " + str(time.time() - start))
+
+        else:
+            self.bot.console_print("---NEEDS TARGETS!---", 'red')
+            with open("NEEDS_TARGETS", "w") as n:
+                n.write("PUT SOME FUCKING TARGETS IN influencers/targets.txt")
+            time.sleep(600)
+            self.followPhase()
+
+    def unfollowPhase(self):
+        pass
+
+class UserLoader(threading.Thread):
+    def __init__(self, l, bot):
+        threading.Thread.__init__(self)
+        self._stop_event = threading.Event()
+        self.list = l
+        self.users = []
+        self.bot = bot
+
+    def run(self):
+        for t in tqdm(self.list):
+            user_id = self.bot.convert_to_user_id(t)
+            if self.bot.check_user(user_id):
+                self.users.append(User(user_id, self.bot.get_user_info(user_id)))
+
+        self.stop()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
 class User(object):
     def __init__(self, id, info):
@@ -142,36 +222,37 @@ class File(object):
 def main():
     print("Running TonnoSubito bot")
     print("Current script's schedule:")
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('-a', type=str, help="Action : follow, unfollow")
+    parser.add_argument('-n', type=int, help="Number to follow", default=1000)
+    args = parser.parse_args()
+    followNum = args.n
     tonno = TonnoBot(username, password, followNum)
     tonno.login()
-    tonno.createFollowers(influencers, toFollow)
-    tonno.run_threaded(tonno.massFollow())
-    """
-    follow_followers_list = bot.read_list_from_file("follow_followers.txt")
-    print("Going to follow followers of:", follow_followers_list, " in ")
-    schedule.every(1).hour.do(run_threaded, stats)
-    schedule.every(8).hours.do(run_threaded, like_hashtags)
-    schedule.every(2).hours.do(run_threaded, like_timeline)
-    schedule.every(1).days.at("16:00").do(run_threaded, like_followers_from_random_user_file)
-    schedule.every(2).days.at("11:00").do(run_threaded, follow_followers)
-    schedule.every(16).hours.do(run_threaded, comment_medias)
-    schedule.every(1).days.at("08:00").do(run_threaded, unfollow_non_followers)
-    schedule.every(12).hours.do(run_threaded, follow_users_from_hastag_file)
-    schedule.every(6).hours.do(run_threaded, comment_hashtag)
-    schedule.every(1).days.at("21:28").do(run_threaded, upload_pictures)
-    schedule.every(4).days.at("07:50").do(run_threaded, put_non_followers_on_blacklist)
-    
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-    
-    tasks_list = []
-    for item in follow_followers_list:
-        tasks_list.append((bot.follow_followers, {'user_id': item, 'nfollows': 500}))
-    
-    for func, arg in tasks_list:
-        func(**arg)
-    """
+    if args.a == "follow":
+        tonno.bot.console_print("Running Follow Phase, DO NOT STOP!", 'green')
+        tonno.followPhase()
+    else:
+        tonno.bot.console_print("Running Unfollow Phase, DO NOT STOP!", 'green')
+        tonno.unfollowPhase()
+    """while True:
+        tonno.bot.console_print("Running Follow Phase, DO NOT STOP!", 'green')
+        tonno.followPhase()
+        tonno.bot.console_print("Going to sleep for 5 days, SAFE TO STOP", 'yellow')
+        time.sleep(10)
+        #time.sleep(432000)
+        tonno.bot.console_print("Running Unfollow Phase, DO NOT STOP!", 'green')
+        tonno.unfollowPhase()
+        tonno.bot.console_print("Going to sleep for 1 day, SAFE TO STOP", 'yellow')
+        time.sleep(10)
+        #time.sleep(86400)"""
 
 if __name__ == '__main__':
     main()
+
+    """
+    SOME DATA
+    Follow rate = 1 follower / 20s
+    Unfollow rate = 1 unfollow / 20s
+    Process user = 1 user / 3s
+    """
